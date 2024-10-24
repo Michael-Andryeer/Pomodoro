d3e9fe67ea1e8f3caf795cc44e787142bbeef3c1
Michael-Andryeer <andryeerm@gmail.com>
2024-10-24 17:58:50 -0300

    Página home

--- pomodoro/src/Router.tsx
+++ pomodoro/src/Router.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { History } from './pages/History'
+import {Home} from './pages/Home/Home'
+import { History } from './pages'
 import { DefaultLayout } from './layouts'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route index element={<Home />} />
+        <Route index element={< Home />} />
         <Route path="history" element={<History />} />
       </Route>
     </Routes>
