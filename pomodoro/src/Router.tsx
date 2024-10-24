c98b3a39735c18e502f66b8e58a2518b3efe73de
Michael-Andryeer <andryeerm@gmail.com>
2024-10-24 11:13:17 -0300

    Consegui resolver o problemas pra rodar o projeto.

--- pomodoro/src/Router.tsx
+++ pomodoro/src/Router.tsx
@@ -1,15 +1,15 @@
-import {Routes,Route} from 'react-router-dom'
-import { Home } from './pages/Home';
-import { History } from './pages/History';
-import { DefaultLayout } from './layouts/DefaultLayout';
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
-  return(
+  return (
     <Routes>
-      <Route path='/' element={<DefaultLayout/>}>
-     <Route path='/' element={<Home/>}/>
-     <Route path='/history' element={<History/>}/>
-     </Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<Home />} />
+        <Route path="history" element={<History />} />
+      </Route>
     </Routes>
-  );
+  )
 }
