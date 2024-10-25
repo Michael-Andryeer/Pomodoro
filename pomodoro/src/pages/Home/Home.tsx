6c9598a39a50062f1d706a675f1870c1a1ca7c00
Michael-Andryeer <andryeerm@gmail.com>
2024-10-25 15:47:11 -0300

    Aprimorando os Inputs

--- pomodoro/src/pages/Home/Home.tsx
+++ pomodoro/src/pages/Home/Home.tsx
@@ -7,10 +7,16 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TasktInput id="task" placeholder='Dê um nome ao seu projeto.' />
+          <TasktInput id="task" list='task-suggestions' placeholder='Dê um nome ao seu projeto.' />
+
+          <datalist id='task-suggestions'>
+            <option value="teste 1" />
+            <option value="teste 2" />
+            <option value="teste 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder='00' />
+          <MinutesAmountInput type="number" id="minutesAmount" placeholder='00' step={5} min={5} max={60} />
 
           <span>minutos.</span>
         </FormContainer>
