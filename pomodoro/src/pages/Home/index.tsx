bcad47d9945698418f8a0ae5341c3e65400b651e
Michael-Andryeer <andryeerm@gmail.com>
2024-10-28 17:11:40 -0300

    React Hook Form

--- pomodoro/src/pages/Home/index.tsx
+++ pomodoro/src/pages/Home/index.tsx
@@ -1,13 +1,24 @@
 import { Play } from 'phosphor-react'
 import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator,StartCountDownButton, TasktInput } from './styles'
+import {useForm} from 'react-hook-form'
+
 
 export function Home() {
+  const {register,handleSubmit,watch} = useForm()
+
+  function handleCreateNewCycle (data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+  
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TasktInput id="task" list='task-suggestions' placeholder='Dê um nome ao seu projeto.' />
+          <TasktInput id="task" list='task-suggestions' placeholder='Dê um nome ao seu projeto.' {...register('task')}/>
 
           <datalist id='task-suggestions'>
             <option value="teste 1" />
@@ -16,7 +27,7 @@ export function Home() {
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder='00' step={5} min={5} max={60} />
+          <MinutesAmountInput type="number" id="minutesAmount" placeholder='00' step={5} min={5} max={60} {...register('minutesAmount',{valueAsNumber: true})} />
 
           <span>minutos.</span>
         </FormContainer>
@@ -29,7 +40,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDownButton type="submit">
+        <StartCountDownButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Iniciar
         </StartCountDownButton>
